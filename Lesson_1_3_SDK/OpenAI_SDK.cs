d754da10f6d2c48adb6220a022920038956e9885
Gilad <gilad@markman.co.il>
2026-02-09 21:24:35 +0200

    update openAI and Gemini SDK

---
+++ Lesson_1_3_SDK/OpenAI_SDK.cs
@@ -0,0 +1,28 @@
+﻿using DotNetEnv;
+using OpenAI.Chat;
+
+namespace Lesson_1_2_http
+{
+    public class OpenAI_SDK
+    {
+        public static async Task<string> Call()
+        {
+            Env.Load(@"C:\Users\Gilad\source\repos\SK\.env");
+            var OpenAIKey = Environment.GetEnvironmentVariable("OpenAIKey");
+            string model = "gpt-5-mini";
+
+            // User prompt message
+            Console.Write("You (program) >> ");
+            var userMessage = Console.ReadLine();
+
+            // Create OpenAI client
+            var client = new ChatClient(model, OpenAIKey);
+
+            // Send the chat completion request
+            var completion = await client.CompleteChatAsync(userMessage);
+
+            // Get the response content
+            return completion.Value.Content[0].Text;
+        }
+    }
+}
