b677d6fc24f983b6aa272e72cb889caeb898927e
Gilad <gilad@markman.co.il>
2026-04-17 10:05:29 +0300

    lesson_8_tools5 - python

---
+++ Lesson_8_Tools5/Program.cs
@@ -0,0 +1,24 @@
+﻿using DotNetEnv;
+
+Env.TraversePath().Load();
+
+//var searchTool = new TavilySearch();
+//var results = await searchTool.Search("Who is Gilad Markman from Israel?");
+//Console.WriteLine(results);
+
+
+// SQLClient
+//var SQLClient = new SQLTools();
+//var table = SQLClient.RetrieveTable("SELECT * FROM GRADES");
+//Console.WriteLine(table);
+
+
+// SQLTools
+
+
+//var tools = new Tools_GPT();
+var tools = new Tools_Gemini();
+//var tools = new Tools_GPT_Thinking();
+//var tools = new Tools_Gemini_Thinking();
+
+await tools.Run();
